c3c435e7e382c9de694e6671861ccada37c6f4b8
theonlytacocat <lt959183@gmail.com>
2026-03-18 14:45:03 -0700

    updating the date for the main page, wa leg update

--- server.js
+++ server.js
@@ -658,7 +658,7 @@ if (fs.existsSync(logFile)) {
     </div>
     <div class="footer">
       <p style="margin-top: 0.5rem;">Monitoring <a href="https://hub.masoncountywa.gov/sheriff/reports/incustdy.pdf" target="_blank">Mason County Jail Roster</a></p>
-      <a href="/legislative" style="display: inline-block; margin-top: 1rem; padding: 0.5rem 1rem; background: #C8C87A; color: #070907; border-radius: 6px; text-decoration: none; font-size: 0.75rem;">March 6th 2026: Washington State Legislative Session News Update</a>
+      <a href="/legislative" style="display: inline-block; margin-top: 1rem; padding: 0.5rem 1rem; background: #C8C87A; color: #070907; border-radius: 6px; text-decoration: none; font-size: 0.75rem;">March 13th 2026: FINAL WA Legislative Session Update</a>
     </div>
   </div>
 </body>
